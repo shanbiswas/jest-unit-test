1e5e4c9339daa6deb33616bf52d1bb00b594467e
Santanu Biswas <shanpro.2015@gmail.com>
2022-11-23 22:09:49 +0530

    Check text present or not

---
+++ src/TextCheck/TextCheck.js
@@ -0,0 +1,10 @@
+
+function TextCheck() {
+  return (
+    <div className="App">
+      <p>Learn Unit Test</p>
+    </div>
+  );
+}
+
+export default TextCheck;
--- src/TextCheck/TextCheck.test.js
+++ src/TextCheck/TextCheck.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import TextCheck from './TextCheck';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<TextCheck />);
+  const linkElement = screen.getByText(/learn unit test/i);
   expect(linkElement).toBeInTheDocument();
 });
